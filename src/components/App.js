1ef90e0a307fe43dc93e8cd8e5407d8f727fc3af
rebeca <rebecachambers919@gmail.com>
2022-11-28 12:42:04 +0100

    add form

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,43 @@ import adalabers from './data/components.json';
 function App() {
   console.log(adalabers);
   const [data, setData] = useState(adalabers);
+  const [newAdalaber, setNewAdalaber] = useState({ 
+    name: '',
+    counselor: '' ,
+    speciality: '' 
+  });
+
+  const handleChangeInput = (ev) => {
+    console.log(ev.target);
+    setNewAdalaber({
+      /*name: newAdalaber.name,
+      counselor: newAdalaber.counselor,
+      speciality: newAdalaber.speciality,
+      */
+      ...newAdalaber, [ev.target.name]: ev.target.value
+    });
+  };
+
+  const handleClickNewAdalaber = (ev) => {
+    /*const allAdalabersClone = [...allAdalabers];
+      allAdalabersClone.push(newAdalaber);*/
+
+    const allAdalabersClone = [...data, newAdalaber];
+    setData(allAdalabersClone);
+    setNewAdalaber ({
+      name: '',
+      counselor: '',
+      speciality: '',
+    });
+
+  };
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+  };
   
-  const htmlData = data.map((adalaber) => {
-    return <tr>
+  const htmlData = data.map((adalaber, index) => {
+    return <tr key={index}>
               <td>{adalaber.name}</td>
               <td>{adalaber.counselor}</td>
               <td>{adalaber.speciality}</td>
@@ -16,27 +50,53 @@ function App() {
 
   return (
     <div className="App">
-    <h1 className="title">Adalabers</h1>
-    <table className="table">
+      <header>
+        <h1 className="title">Adalabers</h1>
+      </header>
+
+      <main>
+        <section>
+          <table className="table">
  
-      <thead><tr>
-      <th>Nombre</th>
-      <th>Tutora</th>
-      <th>Especialidad</th>
-      </tr></thead>
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Tutora</th>
+                <th>Especialidad</th>
+                </tr>
+            </thead>
 
-      <tbody>{htmlData}</tbody>
-    </table>
+            <tbody>{htmlData(adalabers)}</tbody>
+          </table>
 
+        </section>
+        <section>
+          <h2>Añadir una adalaber</h2>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="newName"> Nombre:
+                <input type="text" name="name" id="newName" onChange={handleChangeInput} value={newAdalaber.name}/>
+              </label>
 
+              <label htmlFor="newCounselor"> Tutora:
+              <input type="text" name="counselor" id="newCounselor" onChange={handleChangeInput} value={newAdalaber.counselor}/>
+              </label>
 
+              <label htmlFor="newSpeciality">
+              <input type="text" name="speciality" id="newSpeciality" onChange={handleChangeInput} value={newAdalaber.speciality}/>
+              </label>
 
+              <label htmlFor="newSpeciality">
+              <button onClick={handleClickNewAdalaber}>Añadir una nueva adalaber</button>
+              </label>
 
-    <form action="" className="formulario">
-      <label htmlFor="name">El nombre:</label>
-      <input className="input" type="text" id="name" placeholder="Holis" max-length="" />
-    </form>
+            </form>
+
+        </section>
+
+      </main>
     </div>
+      
+    
   );
 }
 
